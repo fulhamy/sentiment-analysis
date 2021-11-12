df74bdc8fdb24733eb0e3c2ef4c878556d1b2f54
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-11-11 17:40:35 -0800

    Update python.py

--- python.py
+++ python.py
@@ -18,21 +18,20 @@ for i in range(0, 8343244, 1):
         cur = con.cursor()
         read_table = """select index, "Source", title, published_at date,body, "UID", published_by, body, id,subjectivity, polarity from news_log where subjectivity is null and polarity is null and length(body) > 20 order by id asc limit 1"""
         cur.execute(read_table)
-        df = pd.read_sql_query(read_table, con)
         px_data = pd.read_sql_query(read_table, con)
-        # df = df.set_index('date')
-        data = df
+        records = cur.fetchall()
         
-        body_text = TextBlob(px_data.df['body'])
+        for row in records:
         
-        body_text.sentiment.polarity
-        
-        cur.execute('''UPDATE news_log SET subjectivity = body_text.sentiment.subjectivity, polarity = body_text.sentiment.polarity WHERE id = px_data.df['id']''')
-        
-        cur.fetchall()
-        print(cur.fetchall())
-        print(data)
-         # close the communication with the HerokuPostgres
+            body_text = TextBlob(row[5])
+            polarity_score = body_text.sentiment.polarity
+            subjectivity_score = body_text.sentiment.polarity
+            id_number = row[9]
+
+            cur.execute('''UPDATE news_log SET subjectivity = subjectivity_score, polarity = polarity_score WHERE id = id_number''')
+
+
+             # close the communication with the HerokuPostgres
         cur.close()
     except Exception as error:
         print('Could not connect to the Database.')
